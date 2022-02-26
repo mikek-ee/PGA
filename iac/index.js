6aa62dcb181762aa981fac291a7d7a52e5c5cea0
Mike <me@mikek.ee>
2022-02-26 09:49:04 +0000

    sat

--- iac/index.js
+++ iac/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-const { ec2, autoscaling } = require("@pulumi/aws");
+const { ec2, autoscaling, iam } = require("@pulumi/aws");
+const cloudInit = require("@pulumi/cloudinit");
 
 const config = new pulumi.Config();
 
@@ -8,6 +9,8 @@ const instanceType = "t2.micro"
 const availabilityZones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 const appName = config.require("appName")
 const appVer = config.require("appVer")
+const repoURL = config.require("repoURL")
+const keyName = config.require("keyName")
 
 const ami = ec2.getAmi({
     filters: [
@@ -24,55 +27,97 @@ const ami = ec2.getAmi({
     owners: ["309956199498"]
 })
 
-const launchTemplateA = new ec2.LaunchTemplate("launchTemplateA", {
-    namePrefix: "launchTemplateA",
-    imageId: ami.imageId,
-    instanceType,
-    tagSpecifications: [{
-        resourceType: "instance",
-        tags: {
-            appName,
-            appVer,
-            appRole: "clusterA",
-        }
+const sg = new ec2.SecurityGroup("securityGroup", {
+    ingress: [{
+        fromPort: 80,
+        toPort: 80,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"],
+    }, {
+        fromPort: 22,
+        toPort: 22,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"],
     }],
-    userData: "",
+    egress: [{
+        fromPort: 0,
+        toPort: 0,
+        protocol: "-1",
+        cidrBlocks: ["0.0.0.0/0"],
+        ipv6CidrBlocks: ["::/0"],
+    }]
 })
 
-const launchTemplateB = new ec2.LaunchTemplate("launchTemplateB", {
-    namePrefix: "launchTemplateA",
-    imageId: ami.imageId,
-    instanceType,
-    tagSpecifications: [{
-        resourceType: "instance",
-        tags: {
-            appName,
-            appVer,
-            appRole: "clusterA",
+const resourceConf = new cloudInit.Config("cloudinit", {
+    gzip: false,
+    base64Encode: false,
+    parts: [{
+        contentType: "text/x-shellscript",
+        content: "",
+        filename: "/bin/get-aws-tag"
+    }]
+})
+
+ami.then(ami => {
+
+    const launchTemplateA = new ec2.LaunchTemplate("launchTemplateA", {
+        namePrefix: "launchTemplateA",
+        imageId: ami.id,
+        instanceType,
+        keyName,
+        tagSpecifications: [{
+            resourceType: "instance",
+            tags: {
+                appName,
+                appVer,
+                appRole: "clusterA",
+                repoURL,
+            }
+        }],
+        userData: "",
+        vpcSecurityGroupIds: [sg.id],
+    })
+
+    const launchTemplateB = new ec2.LaunchTemplate("launchTemplateB", {
+        namePrefix: "launchTemplateB",
+        imageId: ami.imageId,
+        instanceType,
+        keyName,
+        tagSpecifications: [{
+            resourceType: "instance",
+            tags: {
+                appName,
+                appVer,
+                appRole: "clusterA",
+                repoURL,
+            }
+        }],
+        userData: "",
+        vpcSecurityGroupIds: [sg.id],
+    })
+
+    const asgA = new autoscaling.Group("asgA", {
+        availabilityZones,
+        desiredCapacity: 1,
+        maxSize: 3,
+        minSize: 1,
+        launchTemplate: {
+            id: launchTemplateA.id,
+            version: '$Latest',
         }
-    }],
-    userData: "",
+    })
+
+    const asgB = new autoscaling.Group("asgB", {
+        availabilityZones,
+        desiredCapacity: 1,
+        maxSize: 3,
+        minSize: 1,
+        launchTemplate: {
+            id: launchTemplateB.id,
+            version: '$Latest',
+        }
+    })
+
 })
 
-const asgA = new autoscaling.Group("asgA", {
-    availabilityZones,
-    desiredCapacity: 2,
-    maxSize: 3,
-    minSize: 1,
-    launchTemplate: {
-        id: launchTemplateA.id,
-        version: '$Latest',
-    }
-})
-
-const asgB = new autoscaling.Group("asgB", {
-    availabilityZones,
-    desiredCapacity: 2,
-    maxSize: 3,
-    minSize: 1,
-    launchTemplate: {
-        id: launchTemplateB.id,
-        version: '$Latest',
-    }
-})
 
